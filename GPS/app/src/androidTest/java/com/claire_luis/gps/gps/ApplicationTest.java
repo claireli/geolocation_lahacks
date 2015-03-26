44a2ee6f6d60b95093bc72cff7b255467d3f92b4
Claire Li <cyl053@ucsd.edu>
2015-03-26 15:02:59 -0700

    first push, testing testing

---
+++ GPS/app/src/androidTest/java/com/claire_luis/gps/gps/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.claire_luis.gps.gps;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
