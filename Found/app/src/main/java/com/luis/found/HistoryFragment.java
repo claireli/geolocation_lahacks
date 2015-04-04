9106fa609b385cb6ef1d54e9bccab2365c38806d
lmeraz <lgmeraz@live.com>
2015-04-04 14:29:00 -0700

    First Found Push

---
+++ Found/app/src/main/java/com/luis/found/HistoryFragment.java
@@ -0,0 +1,23 @@
+package com.luis.found;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Luis Meraz on 4/4/2015.
+ */
+public class HistoryFragment extends Fragment {
+    public HistoryFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.history_fragment, container, false);
+        return rootView;
+    }
+}
